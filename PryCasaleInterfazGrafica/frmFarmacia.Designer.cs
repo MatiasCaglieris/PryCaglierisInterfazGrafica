ce6f84e32c0858f449faaea33d2f39a84d8656b1
MatiasCaglieris <matiascaglieris74@gmail.com>
2026-08-20 09:56:10 -0300

    hola

---
+++ PryCasaleInterfazGrafica/frmFarmacia.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace PryCasaleInterfazGrafica
+{
+    partial class frmFarmacia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFarmacia = new System.Windows.Forms.Label();
+            this.labelCantidad = new System.Windows.Forms.Label();
+            this.labelPrecioIva = new System.Windows.Forms.Label();
+            this.labelPrecio = new System.Windows.Forms.Label();
+            this.labelLaboratorio = new System.Windows.Forms.Label();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelRegistro = new System.Windows.Forms.Label();
+            this.buttonAlmacenar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelFarmacia
+            // 
+            this.labelFarmacia.AutoSize = true;
+            this.labelFarmacia.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFarmacia.Location = new System.Drawing.Point(12, 9);
+            this.labelFarmacia.Name = "labelFarmacia";
+            this.labelFarmacia.Size = new System.Drawing.Size(322, 73);
+            this.labelFarmacia.TabIndex = 0;
+            this.labelFarmacia.Text = "Farmacity";
+            // 
+            // labelCantidad
+            // 
+            this.labelCantidad.AutoSize = true;
+            this.labelCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCantidad.Location = new System.Drawing.Point(25, 115);
+            this.labelCantidad.Name = "labelCantidad";
+            this.labelCantidad.Size = new System.Drawing.Size(73, 20);
+            this.labelCantidad.TabIndex = 1;
+            this.labelCantidad.Text = "Cantidad";
+            // 
+            // labelPrecioIva
+            // 
+            this.labelPrecioIva.AutoSize = true;
+            this.labelPrecioIva.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPrecioIva.Location = new System.Drawing.Point(391, 177);
+            this.labelPrecioIva.Name = "labelPrecioIva";
+            this.labelPrecioIva.Size = new System.Drawing.Size(114, 20);
+            this.labelPrecioIva.TabIndex = 2;
+            this.labelPrecioIva.Text = "Precio con IVA";
+            // 
+            // labelPrecio
+            // 
+            this.labelPrecio.AutoSize = true;
+            this.labelPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPrecio.Location = new System.Drawing.Point(391, 115);
+            this.labelPrecio.Name = "labelPrecio";
+            this.labelPrecio.Size = new System.Drawing.Size(73, 20);
+            this.labelPrecio.TabIndex = 3;
+            this.labelPrecio.Text = "Cantidad";
+            // 
+            // labelLaboratorio
+            // 
+            this.labelLaboratorio.AutoSize = true;
+            this.labelLaboratorio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLaboratorio.Location = new System.Drawing.Point(25, 332);
+            this.labelLaboratorio.Name = "labelLaboratorio";
+            this.labelLaboratorio.Size = new System.Drawing.Size(90, 20);
+            this.labelLaboratorio.TabIndex = 4;
+            this.labelLaboratorio.Text = "Laboratorio";
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNombre.Location = new System.Drawing.Point(25, 255);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(65, 20);
+            this.labelNombre.TabIndex = 5;
+            this.labelNombre.Text = "Nombre";
+            // 
+            // labelRegistro
+            // 
+            this.labelRegistro.AutoSize = true;
+            this.labelRegistro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRegistro.Location = new System.Drawing.Point(25, 185);
+            this.labelRegistro.Name = "labelRegistro";
+            this.labelRegistro.Size = new System.Drawing.Size(69, 20);
+            this.labelRegistro.TabIndex = 6;
+            this.labelRegistro.Text = "Registro";
+            // 
+            // buttonAlmacenar
+            // 
+            this.buttonAlmacenar.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.buttonAlmacenar.Location = new System.Drawing.Point(395, 261);
+            this.buttonAlmacenar.Name = "buttonAlmacenar";
+            this.buttonAlmacenar.Size = new System.Drawing.Size(127, 83);
+            this.buttonAlmacenar.TabIndex = 7;
+            this.buttonAlmacenar.Text = "Almacenar";
+            this.buttonAlmacenar.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 185);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(117, 247);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(117, 324);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(511, 112);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(511, 179);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // frmFarmacia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonAlmacenar);
+            this.Controls.Add(this.labelRegistro);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.labelLaboratorio);
+            this.Controls.Add(this.labelPrecio);
+            this.Controls.Add(this.labelPrecioIva);
+            this.Controls.Add(this.labelCantidad);
+            this.Controls.Add(this.labelFarmacia);
+            this.Name = "frmFarmacia";
+            this.Text = "Farmacia Farmacity";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFarmacia;
+        private System.Windows.Forms.Label labelCantidad;
+        private System.Windows.Forms.Label labelPrecioIva;
+        private System.Windows.Forms.Label labelPrecio;
+        private System.Windows.Forms.Label labelLaboratorio;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label labelRegistro;
+        private System.Windows.Forms.Button buttonAlmacenar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
