df6bb964411cd2418311a34c27b33ee9683e1cea
MatiasCaglieris <matiascaglieris74@gmail.com>
2026-09-14 11:32:12 -0300

    interfaz gestor de tareas caso 8

---
+++ PryCasaleInterfazGrafica/GestordeTareas.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace PryCasaleInterfazGrafica
+{
+    partial class GestordeTareas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblTarea = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAgregarTarea = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnCompletar = new System.Windows.Forms.Button();
+            this.txtTarea = new System.Windows.Forms.TextBox();
+            this.cmbPrioridad = new System.Windows.Forms.ComboBox();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.chkboxtp = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.lblAlta = new System.Windows.Forms.Label();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.lblbaja = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(287, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(130, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "GESTION DE FARMACIA";
+            // 
+            // lblTarea
+            // 
+            this.lblTarea.AutoSize = true;
+            this.lblTarea.Location = new System.Drawing.Point(32, 65);
+            this.lblTarea.Name = "lblTarea";
+            this.lblTarea.Size = new System.Drawing.Size(49, 13);
+            this.lblTarea.TabIndex = 1;
+            this.lblTarea.Text = "TAREA: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PRIORIDAD: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "FECHA: ";
+            // 
+            // btnAgregarTarea
+            // 
+            this.btnAgregarTarea.Location = new System.Drawing.Point(74, 164);
+            this.btnAgregarTarea.Name = "btnAgregarTarea";
+            this.btnAgregarTarea.Size = new System.Drawing.Size(135, 31);
+            this.btnAgregarTarea.TabIndex = 4;
+            this.btnAgregarTarea.Text = "[ AGREGAR TAREA ] ";
+            this.btnAgregarTarea.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(50, 337);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(117, 59);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnCompletar
+            // 
+            this.btnCompletar.Location = new System.Drawing.Point(220, 337);
+            this.btnCompletar.Name = "btnCompletar";
+            this.btnCompletar.Size = new System.Drawing.Size(117, 59);
+            this.btnCompletar.TabIndex = 7;
+            this.btnCompletar.Text = "COMPLETAR";
+            this.btnCompletar.UseVisualStyleBackColor = true;
+            // 
+            // txtTarea
+            // 
+            this.txtTarea.Location = new System.Drawing.Point(182, 65);
+            this.txtTarea.Name = "txtTarea";
+            this.txtTarea.Size = new System.Drawing.Size(100, 20);
+            this.txtTarea.TabIndex = 8;
+            // 
+            // cmbPrioridad
+            // 
+            this.cmbPrioridad.FormattingEnabled = true;
+            this.cmbPrioridad.Location = new System.Drawing.Point(182, 97);
+            this.cmbPrioridad.Name = "cmbPrioridad";
+            this.cmbPrioridad.Size = new System.Drawing.Size(121, 21);
+            this.cmbPrioridad.TabIndex = 9;
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(182, 129);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha.TabIndex = 10;
+            // 
+            // chkboxtp
+            // 
+            this.chkboxtp.AutoSize = true;
+            this.chkboxtp.Location = new System.Drawing.Point(50, 218);
+            this.chkboxtp.Name = "chkboxtp";
+            this.chkboxtp.Size = new System.Drawing.Size(108, 17);
+            this.chkboxtp.TabIndex = 11;
+            this.chkboxtp.Text = "TP Programacion";
+            this.chkboxtp.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(50, 250);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(81, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Estudiar C#";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(50, 283);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(104, 17);
+            this.checkBox2.TabIndex = 13;
+            this.checkBox2.Text = "Entregar Informe";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // lblAlta
+            // 
+            this.lblAlta.AutoSize = true;
+            this.lblAlta.Location = new System.Drawing.Point(287, 222);
+            this.lblAlta.Name = "lblAlta";
+            this.lblAlta.Size = new System.Drawing.Size(25, 13);
+            this.lblAlta.TabIndex = 14;
+            this.lblAlta.Text = "Alta";
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.AutoSize = true;
+            this.lblMedia.Location = new System.Drawing.Point(287, 250);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(36, 13);
+            this.lblMedia.TabIndex = 15;
+            this.lblMedia.Text = "Media";
+            // 
+            // lblbaja
+            // 
+            this.lblbaja.AutoSize = true;
+            this.lblbaja.Location = new System.Drawing.Point(287, 283);
+            this.lblbaja.Name = "lblbaja";
+            this.lblbaja.Size = new System.Drawing.Size(28, 13);
+            this.lblbaja.TabIndex = 16;
+            this.lblbaja.Text = "Baja";
+            // 
+            // GestordeTareas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblbaja);
+            this.Controls.Add(this.lblMedia);
+            this.Controls.Add(this.lblAlta);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.chkboxtp);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.cmbPrioridad);
+            this.Controls.Add(this.txtTarea);
+            this.Controls.Add(this.btnCompletar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregarTarea);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTarea);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "GestordeTareas";
+            this.Text = "COMPLETAR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblTarea;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAgregarTarea;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnCompletar;
+        private System.Windows.Forms.TextBox txtTarea;
+        private System.Windows.Forms.ComboBox cmbPrioridad;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.CheckBox chkboxtp;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label lblAlta;
+        private System.Windows.Forms.Label lblMedia;
+        private System.Windows.Forms.Label lblbaja;
+    }
+}
--- PryCasaleInterfazGrafica/frmSistemasdeVentas.Designer.cs
+++ PryCasaleInterfazGrafica/frmSistemasdeVentas.Designer.cs
@@ -28,19 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.txtProducto = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblTotalN = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(144, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(118, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "SISTEMA DE VENTAS";
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(12, 57);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(56, 13);
+            this.lblProducto.TabIndex = 1;
+            this.lblProducto.Text = "Producto: ";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(12, 98);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(43, 13);
+            this.lblPrecio.TabIndex = 2;
+            this.lblPrecio.Text = "Precio: ";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(12, 140);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(55, 13);
+            this.lblCantidad.TabIndex = 3;
+            this.lblCantidad.Text = "Cantidad: ";
+            // 
+            // txtProducto
+            // 
+            this.txtProducto.Location = new System.Drawing.Point(124, 50);
+            this.txtProducto.Name = "txtProducto";
+            this.txtProducto.Size = new System.Drawing.Size(177, 20);
+            this.txtProducto.TabIndex = 4;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(124, 91);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 5;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(124, 133);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 6;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(160, 176);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(87, 52);
+            this.btnAgregar.TabIndex = 7;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(12, 417);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(48, 13);
+            this.lblTotal.TabIndex = 8;
+            this.lblTotal.Text = "TOTAL: ";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 262);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(406, 108);
+            this.listBox1.TabIndex = 9;
+            // 
+            // lblTotalN
+            // 
+            this.lblTotalN.AutoSize = true;
+            this.lblTotalN.Location = new System.Drawing.Point(358, 416);
+            this.lblTotalN.Name = "lblTotalN";
+            this.lblTotalN.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalN.TabIndex = 10;
             // 
             // frmSistemasdeVentas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(399, 450);
+            this.ClientSize = new System.Drawing.Size(433, 450);
+            this.Controls.Add(this.lblTotalN);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtProducto);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblProducto);
+            this.Controls.Add(this.lblTitulo);
             this.Name = "frmSistemasdeVentas";
-            this.Text = "frmSistemasdeVentas";
+            this.Text = "Producto: ";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.TextBox txtProducto;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblTotalN;
     }
 }
